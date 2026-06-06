ba75b3ac6193118bce5cf6b1f900be153e8c1d1d
Isa Yan <isabel.yyj.yan@gmail.com>
2026-06-05 23:58:58 -0400

    first commit

---
+++ index.cpp
@@ -0,0 +1,18 @@
+#include <iostream>;
+#include <string>;
+#include <vector>;
+#include <algorithm>;
+#include <random>;
+#include <ctime>;
+#include <cstdlib>;
+using namespace std;
+
+int main(){
+    srand(time(0));
+    int random_num;
+
+    std::cout<<random_num;
+    
+
+
+}
