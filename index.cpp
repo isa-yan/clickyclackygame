44e0db094b5501287894012ea9d2732f4892caae
Isa Yan <isabel.yyj.yan@gmail.com>
2026-06-08 23:15:53 -0400

    mraow

--- index.cpp
+++ index.cpp
@@ -1,18 +1,24 @@
-#include <iostream>;
-#include <string>;
-#include <vector>;
-#include <algorithm>;
-#include <random>;
-#include <ctime>;
-#include <cstdlib>;
-using namespace std;
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <random>
+
+/*
+clang++ -g index.cpp -o index
+./index
+*/
 
 int main(){
-    srand(time(0));
-    int random_num;
+    std::string meows[4] = {"mraow","meow","mrrp","purr"};
+    std::string cpp_terms[4] = {"std::string","std::cout","#include","int main(){}"};
+    char x;
 
-    std::cout<<random_num;
-    
+    std::cout<<"what selection of words would you like to try your speed out :3 "<<"\n";
+    std::cout<<"insert 'meow' if you want cats. insert 'cpp vocab' if you want cpp vocab >:3"<<"\n";
+    std::cin>>x;
+    if (x=='m')
+        std::cout<<"so meow it is! :3";
 
 
 }
